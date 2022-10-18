f634121476e032f123e95c26032116517b493c27
TiagoArau <tiagoaraujosj@gmail.com>
2022-10-18 19:13:21 -0300

    Correção parcial  dos parametros do javascript

--- js.js
+++ js.js
@@ -1,58 +1,64 @@
-function agua() {
+
+function percentageluz() {
   
-      var num1 = document.getElementById("agua").value;
+   
+    var num1 = document.getElementById("luz").value;
       
-       var num2 = document.getElementById("salario").value;
-    document.getElementById("%agua")
-        .value = (num1 * 100) / num2 + "%";
-
-}
-function luz() {
-
-        var num3 = document.getElementById("luz").value;
-      
-        var num4 = document.getElementById("salario").value;
-     document.getElementById("%luz")
-     .value = (num3 * 100) / num4 + "%";
-}
-function caredemoveis() {
-       
-
-    var num5 = document.getElementById("redemoveis").value;
- 
-   var num6 = document.getElementById("salario").value;
-document.getElementById("redemoveisresposta")
-    .value = (num5 * 100) / num6 + "%";
     
+    var num2 = document.getElementById("salario").value;
+    document.getElementById("value1")
+        .value = (num1 * 100) / num2 + "%";
+}
+
+
+function pagua() {
+  
+    var num3 = document.getElementById("agua").value;
+    
+     var num2 = document.getElementById("salario").value;
+  document.getElementById("cagua")
+      .value = (num3 * 100) / num2 + "%";
+
+}
+
+function caredemoveis() {
+     
+
+  var num5 = document.getElementById("redemoveis").value;
+
+ var num6 = document.getElementById("salario").value;
+document.getElementById("redemoveisresposta")
+  .value = (num5 * 100) / num6 + "%";
+  
 
 } 
 
 function internet() {
 
-         var num7 = document.getElementById("internet").value;
-      
-        var num8= document.getElementById("salario").value;
-     document.getElementById("%internet")
-         .value = (num7 * 100) / num8 + "%";
+       var num7 = document.getElementById("internet").value;
+    
+      var num8= document.getElementById("salario").value;
+   document.getElementById("%internet")
+       .value = (num7 * 100) / num8 + "%";
 }
 function condominio() {
-         var num9 = document.getElementById("condominio").value;
-      
-         var num10= document.getElementById("salario").value;
-      document.getElementById("%condominio")
-          .value = (num9 * 100) / num10 + "%";
+       var num9 = document.getElementById("condominio").value;
+    
+       var num10= document.getElementById("salario").value;
+    document.getElementById("%condominio")
+        .value = (num9 * 100) / num10 + "%";
 }
 function aluguel() {
-          var num11 = document.getElementById("aluguel").value;
-      
-          var num12= document.getElementById("salario").value;
-       document.getElementById("%aluguel")
-           .value = (num11 * 100) / num12 + "%";
+        var num11 = document.getElementById("aluguel").value;
+    
+        var num12= document.getElementById("salario").value;
+     document.getElementById("%aluguel")
+         .value = (num11 * 100) / num12 + "%";
 }
 function conducao() {
-           var num13 = document.getElementById("conducao").value;
-      
-           var num14= document.getElementById("salario").value;
-        document.getElementById("%conducao")
-            .value = (num13 * 100) / num14 + "%";
+         var num13 = document.getElementById("conducao").value;
+    
+         var num14= document.getElementById("salario").value;
+      document.getElementById("%conducao")
+          .value = (num13 * 100) / num14 + "%";
 }
