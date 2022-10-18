4d08b9ef524a862ca0a8a11408bb00e48f6c5c07
TiagoArau <tiagoaraujosj@gmail.com>
2022-10-18 17:44:40 -0300

    precisa salvar o comando java script

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-function percentage() {
+function agua() {
   
       var num1 = document.getElementById("agua").value;
       
@@ -6,36 +6,50 @@ function percentage() {
     document.getElementById("%agua")
         .value = (num1 * 100) / num2 + "%";
 
+}
+function luz() {
+
         var num3 = document.getElementById("luz").value;
       
         var num4 = document.getElementById("salario").value;
      document.getElementById("%luz")
-         .value = (num3 * 100) / num4 + "%";
+     .value = (num3 * 100) / num4 + "%";
+}
+function caredemoveis() {
+       
 
-         var num5 = document.getElementById("redemoveis").value;
-      
-        var num6 = document.getElementById("salario").value;
-     document.getElementById("%redemoveis")
-         .value = (num5 * 100) / num6 + "%";
+    var num5 = document.getElementById("redemoveis").value;
+ 
+   var num6 = document.getElementById("salario").value;
+document.getElementById("redemoveisresposta")
+    .value = (num5 * 100) / num6 + "%";
+    
+
+} 
+
+function internet() {
 
          var num7 = document.getElementById("internet").value;
       
         var num8= document.getElementById("salario").value;
      document.getElementById("%internet")
          .value = (num7 * 100) / num8 + "%";
-
+}
+function condominio() {
          var num9 = document.getElementById("condominio").value;
       
          var num10= document.getElementById("salario").value;
       document.getElementById("%condominio")
           .value = (num9 * 100) / num10 + "%";
-
+}
+function aluguel() {
           var num11 = document.getElementById("aluguel").value;
       
           var num12= document.getElementById("salario").value;
        document.getElementById("%aluguel")
            .value = (num11 * 100) / num12 + "%";
-
+}
+function conducao() {
            var num13 = document.getElementById("conducao").value;
       
            var num14= document.getElementById("salario").value;
